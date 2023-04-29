fce1e8c06a525c051e4306fcdb16c49d450fbc9a
Detroit98 <duruakuebuka@gmail.com>
2023-04-29 20:26:19 +0100

    fetch all users

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
-
+import { BASE_URL } from "@/utils";
 const useAuthStore = create(
   persist(
     (set, get) => ({
       userProfile: null,
       addUser: (user: any) => set({ userProfile: user }),
       removeUser: () => set({ userProfile: null }),
+      fetchAllUser: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`);
+      },
     }),
     {
       name: "auth",
